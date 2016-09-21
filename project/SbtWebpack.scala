b5f763ac834d990151aa7287ece0041d66f4908c
Julien Richard-Foy <julien@richard-foy.fr>
2016-09-21 18:13:01 +0200

    basic support of fullOptJS

--- project/SbtWebpack.scala
+++ project/SbtWebpack.scala
@@ -1,14 +1,16 @@
 import org.scalajs.core.ir.Utils
 import sbt._
 import sbt.Keys._
-import org.scalajs.sbtplugin.ScalaJSPlugin.AutoImport.fastOptJS
+import org.scalajs.sbtplugin.ScalaJSPlugin.AutoImport.{fastOptJS, fullOptJS}
 
 object SbtWebpack extends AutoPlugin {
 
   object autoImport {
     val npmDependencies = settingKey[Map[String, String]]("NPM dependencies")
 
-    val bundle = taskKey[File]("Bundle the application")
+    val bundle = taskKey[File]("Bundle the output of the fastOptJS task")
+
+    val bundleOpt = taskKey[File]("Bundle the output of the fullOptJS task")
   }
 
   import autoImport._
@@ -20,49 +22,49 @@ object SbtWebpack extends AutoPlugin {
   private val perConfigSettings: Seq[Def.Setting[_]] =
     Seq(
       npmDependencies := Map.empty,
-
-      bundle := Def.taskDyn {
-        val stage = fastOptJS // TODO support fullopt too
-        Def.task {
-          val log = streams.value.log
-          val targetDir = (crossTarget in stage).value
-          val deps = npmDependencies.value
-
-          val stageOutput = stage.value.data
-
-          // Create a launcher (TODO remove as soon as they will disappear from Scala.js)
-          val mainFqp =
-            mainClass.value.getOrElse(sys.error("No main class detected"))
-              .split('.')
-              .map(p => s"""["${Utils.escapeJS(p)}"]""")
-              .mkString
-          val launcherContent =
-            s"""
-              |require("${Utils.escapeJS(stageOutput.absolutePath)}")$mainFqp().main();
-            """.stripMargin
-          val launcherFile = targetDir / "launcher.js"
-          IO.write(launcherFile, launcherContent)
-
-          // Create a package.json file
-          val depsJson =
-            (for ((name, version) <- deps) yield s""" "$name": "$version" """)
-              .mkString("{", ",", "}")
-          val packageJson =
-            s"""{
-                |  "dependencies": $depsJson,
-                |  "devDependencies": { "webpack": "1.13" },
-                |  "scripts": {
-                |    "bundle": "webpack ${launcherFile.absolutePath} app.js"
-                |  }
-                |}""".stripMargin
-          IO.write(targetDir / "package.json", packageJson)
-          val process =
-            Process("npm update", targetDir) #&&
-            Process("npm run bundle", targetDir)
-          process.run(log).exitValue()
-          targetDir
-        }
-      }.value
+      bundle := Def.taskDyn(bundleTask(fastOptJS)).value,
+      bundleOpt := Def.taskDyn(bundleTask(fullOptJS)).value
     )
+
+  private def bundleTask(stage: TaskKey[Attributed[File]]): Def.Initialize[Task[File]] =
+    Def.task {
+      val log = streams.value.log
+      val targetDir = (crossTarget in stage).value
+      val deps = npmDependencies.value
+
+      val stageOutput = stage.value.data
+
+      // Create a launcher (TODO remove as soon as they will disappear from Scala.js)
+      val mainFqp =
+      mainClass.value.getOrElse(sys.error("No main class detected"))
+        .split('.')
+        .map(p => s"""["${Utils.escapeJS(p)}"]""")
+        .mkString
+      val launcherContent =
+        s"""
+           |require("${Utils.escapeJS(stageOutput.absolutePath)}")$mainFqp().main();
+            """.stripMargin
+      val launcherFile = targetDir / "launcher.js"
+      IO.write(launcherFile, launcherContent)
+
+      // Create a package.json file
+      val depsJson =
+      (for ((name, version) <- deps) yield s""" "$name": "$version" """)
+        .mkString("{", ",", "}")
+      val packageJson =
+        s"""{
+            |  "dependencies": $depsJson,
+            |  "devDependencies": { "webpack": "1.13" },
+            |  "scripts": {
+            |    "bundle": "webpack ${launcherFile.absolutePath} app.js"
+            |  }
+            |}""".stripMargin
+      IO.write(targetDir / "package.json", packageJson)
+      val process =
+        Process("npm update", targetDir) #&&
+          Process("npm run bundle", targetDir)
+      process.run(log).exitValue()
+      targetDir
+    }
 
 }
