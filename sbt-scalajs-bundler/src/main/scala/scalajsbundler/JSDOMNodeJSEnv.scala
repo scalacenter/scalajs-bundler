8a98880594eca8ecffd490641a778a9f3d5c1307
Julien Richard-Foy <julien@richard-foy.fr>
2016-12-02 14:26:22 +0100

    Support history API within jsdom

--- sbt-scalajs-bundler/src/main/scala/scalajsbundler/JSDOMNodeJSEnv.scala
+++ sbt-scalajs-bundler/src/main/scala/scalajsbundler/JSDOMNodeJSEnv.scala
@@ -71,6 +71,7 @@ class JSDOMNodeJSEnv(
            |    },
            |    scripts: [${scriptsStringPath.mkString(", ")}],
            |    onload: function (window) {
+           |      jsdom.changeURL(window, "http://localhost");
            |      for (var k in window) {
            |        if (windowKeys.indexOf(k) == -1)
            |          global[k] = window[k];
