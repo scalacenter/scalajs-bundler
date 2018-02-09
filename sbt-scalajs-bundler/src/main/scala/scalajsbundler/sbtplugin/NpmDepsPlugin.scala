54580b5b3399df248de09ef5043d19ea7249e3db
Mathieu <mathieu.leclaire@iscpif.fr>
2018-02-09 14:06:11 +0100

    Remove none generic stuff

--- sbt-scalajs-bundler/src/main/scala/scalajsbundler/sbtplugin/NpmDepsPlugin.scala
+++ sbt-scalajs-bundler/src/main/scala/scalajsbundler/sbtplugin/NpmDepsPlugin.scala
@@ -1,13 +1,11 @@
 package scalajsbundler.sbtplugin
 
 import org.scalajs.core.tools.io.FileVirtualJSFile
-import org.scalajs.core.tools.jsdep.ResolvedJSDependency
 import org.scalajs.sbtplugin.ScalaJSPlugin
 import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
 import sbt.Keys._
 import sbt._
 
-import scalajsbundler.NpmDeps.NpmDeps
 import scalajsbundler._
 
 object NpmDepsPlugin extends AutoPlugin {
@@ -54,12 +52,6 @@ object NpmDepsPlugin extends AutoPlugin {
       */
     val useYarn: SettingKey[Boolean] =
       settingKey[Boolean]("Whether to use yarn for updates")
-
-    val npmDeps = settingKey[NpmDeps]("List of js dependencies to be fetched")
-
-    val allNpmDeps = taskKey[NpmDeps]("json file containing all npm js dependencies collected from all dependencies")
-
-    val dependencyFile = taskKey[File]("File containing all external js files")
   }
 
   import autoImport._
@@ -70,49 +62,15 @@ object NpmDepsPlugin extends AutoPlugin {
       KeyRanks.Invisible
     )
 
-  override lazy val projectSettings = Seq(
-    npmDeps in Compile := List.empty,
+  override lazy val projectSettings =
+    perScalaJSStageSettings(fullOptJS) ++ perScalaJSStageSettings(fastOptJS)
 
-    skip in packageJSDependencies := false,
-
-    resolvedJSDependencies in Compile := {
-      val logger = streams.value.log
-      val prev = (resolvedJSDependencies in Compile).value
-
-      // Fetch the js paths in node_modules
-      val jss = {
-        val nodeModules = (npmUpdate in Compile).value / "node_modules"
-
-        (for {
-          m <- (allNpmDeps in Compile).value
-          js <- m.jsFiles
-        } yield {
-          logger.info(s"Fetch $js in ${nodeModules / m.module}")
-          get(nodeModules / m.module, js)
-        }).flatten
-
-      }
-
-      val resolvedDependencies = jss.map { f =>
-        ResolvedJSDependency.minimal(FileVirtualJSFile(f))
-      }
-
-      prev.map(_ ++ resolvedDependencies)
-    },
-
-    dependencyFile := (packageMinifiedJSDependencies in Compile).value,
-
-    (products in Compile) := (products in Compile).dependsOn(npmDepsManifest).value
-  ) ++ perScalaJSStageSettings(fullOptJS) ++ perScalaJSStageSettings(fastOptJS)
-
-  def perScalaJSStageSettings(stage: TaskKey[Attributed[File]]): Seq[Def.Setting[_]] = Seq(
+  protected def perScalaJSStageSettings(stage: TaskKey[Attributed[File]]): Seq[Def.Setting[_]] = Seq(
     useYarn := false
   ) ++ inConfig(Compile)(perConfigSettings)
 
 
   private lazy val perConfigSettings: Seq[Def.Setting[_]] = Seq(
-    allNpmDeps := NpmDeps.collectFromClasspath((fullClasspath in Compile).value),
-
     npmUpdate := {
       val log = streams.value.log
       val targetDir = (crossTarget in npmUpdate).value
@@ -142,42 +100,8 @@ object NpmDepsPlugin extends AutoPlugin {
         }.to[Set])
 
       targetDir
-    },
-
-    scalaJSBundlerPackageJson := PackageJsonTasks.writePackageJson(
-      (crossTarget in npmUpdate).value,
-      (allNpmDeps in Compile).value.map { dep => dep.module -> dep.version },
-      Seq(),
-      Map.empty,
-      Map.empty,
-      fullClasspath.value,
-      configuration.value,
-      "3",
-      "2.7.1",
-      streams.value
-    )
+    }
 
   )
-
-  private def recursiveListFiles(f: File): Array[File] = {
-    val these = f.listFiles
-    these ++ these.filter(_.isDirectory).flatMap(recursiveListFiles)
-  }
-
-  private def get(path: File, jsFile: String) = {
-    val files = recursiveListFiles(path)
-    files.find(_.getName == jsFile)
-  }
-
-  /**
-    * Writes the NpmDeps manifest file.
-    */
-  val npmDepsManifest: Def.Initialize[Task[File]] =
-    Def.task {
-      scalajsbundler.NpmDeps.writeNpmDepsJson(
-        (npmDeps in Compile).value,
-        (classDirectory in Compile).value / NpmDeps.manifestFileName
-      )
-    }
 
 }
