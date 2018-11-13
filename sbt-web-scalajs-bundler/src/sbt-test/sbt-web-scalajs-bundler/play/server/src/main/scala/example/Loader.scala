8dec63f62dbe845f6821c9d22ca498a3a14587a5
Mustafa Kuscu <mustafakuscu@gmail.com>
2018-11-13 10:02:57 +0300

    use AssetsComponents and HttpFiltersComponents, instead of manual instantiation

--- sbt-web-scalajs-bundler/src/sbt-test/sbt-web-scalajs-bundler/play/server/src/main/scala/example/Loader.scala
+++ sbt-web-scalajs-bundler/src/sbt-test/sbt-web-scalajs-bundler/play/server/src/main/scala/example/Loader.scala
@@ -1,19 +1,16 @@
 package example
 
-import controllers.{Assets, AssetsConfiguration, DefaultAssetsMetadata}
+import controllers.AssetsComponents
 import play.api.ApplicationLoader.Context
 import play.api.{ApplicationLoader, BuiltInComponentsFromContext}
+import play.filters.HttpFiltersComponents
 import router.Routes
 
 class Loader extends ApplicationLoader {
   def load(context: Context) = new ExampleComponents(context).application
 }
 
-class ExampleComponents(context: Context) extends BuiltInComponentsFromContext(context) {
-  val httpFilters = Seq()
+class ExampleComponents(context: Context) extends BuiltInComponentsFromContext(context) with AssetsComponents with HttpFiltersComponents {
   val controller = new ExampleController
-  val assetsConfiguration = new AssetsConfiguration()
-  val assetsMetadata = new DefaultAssetsMetadata(assetsConfiguration, environment.resource _, fileMimeTypes)
-  val assets = new Assets(httpErrorHandler, assetsMetadata)
   val router = new Routes(httpErrorHandler, controller, assets)
 }
